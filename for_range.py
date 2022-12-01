119dafc12dd58dd16887c50943484d30fb05af84
joaornj <joaornj@gmail.com>
2022-12-01 09:28:18 -0300

    aula_strings

--- for_range.py
+++ for_range.py
@@ -1,4 +1,4 @@
 
 
-for i in range(10):
+for i in range(10): # neste caso conta até 10
 	print(i)
---
+++ strings.py
@@ -0,0 +1,15 @@
+
+a = "João"
+b = "Nogueira"
+
+concatenar = a + " " + b  # Usa as aspas espacadas para dar espaco entre as duas strings
+print (concatenar) 
+
+tamanho = len(concatenar) # len traz o tamanho da string / o espaço tmbm é somado
+print(tamanho)
+
+print(a[0])   # Irá printar a posicao 0 da string
+
+print(concatenar[0:3])  # Irá printar até a posicao 3 da string
+
+print(concatenar[0:])  # Irá printar tudo
